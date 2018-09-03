fa15a0d4ac765642002103e662ca4f663d9b8fed
Sam Broomfield <sambroomfield@gmail.com>
2018-09-03 12:06:59 +0100

    Cleaned Up File

--- data_analyser.py
+++ data_analyser.py
@@ -10,11 +10,8 @@ class DataAnalyser:
     'Analyses the data found from the Web Scraper'
 
     def __init__(self, folder_name):
-        """
-        for file in os.listdir("BBC-NEWS"):
-            if file.endswith(".txt"):
-                print(os.path.join(file))#"/mydir", file))
-        """
+
+        i = 0
         stop_words = set(stopwords.words('english'))
         word_dict = {}
         for root, dirs, files in os.walk(folder_name):
@@ -22,54 +19,31 @@ class DataAnalyser:
                 if file.lower().endswith(".txt") and file.lower():#.startswith("main"):
                     print(os.path.join(root, file))
                     try:
-                        with open(os.path.join(root, file), 'r') as content_file:
+                        with open(os.path.join(root, file), 'r', encoding='utf8') as content_file:
                             head = list(islice(content_file, 3))
-                            print(head[2].strip(), "\n\n")
+                            print(head[2].strip(), "\n")
                             content = ''.join([head[2], content_file.read()])
-                            #print(content)
                         word_tokens = word_tokenize(content)
                         for word in word_tokens:
-                            if word.lower() not in stop_words and word.isalpha():
+                            word = word.lower()
+                            if word not in stop_words and word.isalpha():
                                 if word not in word_dict:
                                     word_dict[word] = 1
                                 else:
                                     word_dict[word] += 1
                     except IndexError as e:
                         print("Something went wrong:\n{}".format(e))
-
+                    else:
+                        i +=1
+        
         sorted_x = sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True)
         for x in sorted_x:
-            if x[1] < 25:
+            if x[1] < 10:
                 break
             
-            #print("{0} - {1}\t".format(x[0], x[1]), end='')
-        
-                    #for w in sorted_x:
+            print("{0} - {1}\t".format(x[0], x[1]), end='')
+        print('\n\nNumber of pages searched: {}'.format(i))
 
-        #print(sorted_x)
-
-                    #exit()
-                
-        
-"""
-        example_sent = "--{{[This 'is' A sample \"sentence\", showing off the stop words filtration."
-        #example_sent.startswith("")
-        stop_words = set(stopwords.words('english'))
-        print(example_sent, "\n\n")
-        word_tokens = word_tokenize(example_sent)
-        print(word_tokens, "\n\n")
-        
-        filtered_sentence = [w for w in word_tokens if not w in stop_words]
-        print(filtered_sentence, "\n\n")
-        filtered_sentence = []
-        
-        for word in word_tokens: 
-            if word.lower() not in stop_words and word.isalpha():
-                filtered_sentence.append(word)
-        
-        print(word_tokens)
-        print(filtered_sentence)
-"""
 
 if __name__ == '__main__':
     print ("DataAnalyser.__module__:\t{}".format(DataAnalyser.__module__).expandtabs(2))
